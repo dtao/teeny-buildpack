973d326f6ba4bf1f75da03258fb8f19dee560f0e
Dan Tao <daniel.tao@gmail.com>
2014-06-28 15:51:13 -0700

    switched create_database_yml to create_procfile

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -91,7 +91,7 @@ class LanguagePack::Ruby < LanguagePack::Base
       allow_git do
         install_bundler_in_app
         build_bundler
-        create_database_yml
+        create_procfile
         install_binaries
         run_assets_precompile_rake_task
       end
@@ -581,67 +581,12 @@ ERROR
   end
 
   # writes ERB based database.yml for Rails. The database.yml uses the DATABASE_URL from the environment during runtime.
-  def create_database_yml
-    instrument 'ruby.create_database_yml' do
-      log("create_database_yml") do
-        return unless File.directory?("config")
-        topic("Writing config/database.yml to read from DATABASE_URL")
-        File.open("config/database.yml", "w") do |file|
-          file.puts <<-DATABASE_YML
-<%
-
-require 'cgi'
-require 'uri'
-
-begin
-  uri = URI.parse(ENV["DATABASE_URL"])
-rescue URI::InvalidURIError
-  raise "Invalid DATABASE_URL"
-end
-
-raise "No RACK_ENV or RAILS_ENV found" unless ENV["RAILS_ENV"] || ENV["RACK_ENV"]
-
-def attribute(name, value, force_string = false)
-  if value
-    value_string =
-      if force_string
-        '"' + value + '"'
-      else
-        value
-      end
-    "\#{name}: \#{value_string}"
-  else
-    ""
-  end
-end
-
-adapter = uri.scheme
-adapter = "postgresql" if adapter == "postgres"
-
-database = (uri.path || "").split("/")[1]
-
-username = uri.user
-password = uri.password
-
-host = uri.host
-port = uri.port
-
-params = CGI.parse(uri.query || "")
-
-%>
-
-<%= ENV["RAILS_ENV"] || ENV["RACK_ENV"] %>:
-  <%= attribute "adapter",  adapter %>
-  <%= attribute "database", database %>
-  <%= attribute "username", username %>
-  <%= attribute "password", password, true %>
-  <%= attribute "host",     host %>
-  <%= attribute "port",     port %>
-
-<% params.each do |key, value| %>
-  <%= key %>: <%= value.first %>
-<% end %>
-        DATABASE_YML
+  def create_procfile
+    instrument 'ruby.create_procfile' do
+      log("create_procfile") do
+        topic("Writing Procfile")
+        File.open("Procfile", "w") do |file|
+          file.puts "web: bundle exec teeny"
         end
       end
     end
